9513c600cb7cc543d38e825052f2f0df086edf31
Devyash Saini <dysaini2004@gmail.com>
2024-08-02 19:54:18 +0530

    lil fixes

--- src/components/committee/Gallery.jsx
+++ src/components/committee/Gallery.jsx
@@ -13,7 +13,7 @@ const Gallery = () => {
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <div className="inline-block rounded-lg bg-white px-3 py-1 text-sm">
-              Gallery
+              Sneak Peeks
             </div>
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
               Explore Our Gallery
